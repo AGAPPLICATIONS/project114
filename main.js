b5275ae6e040628129f0bbd3438f9d7f5979ed69
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-03-10 18:31:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
-canvas= createCanvas(750,400);
-canvas.position(250,300);
-video=createCapture(VIDEO);
-video.hide();
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.position(425,200);
+    video = createCapture(VIDEO);
+    video.hide();
+    posenet = ml5.poseNet(video, modelloaded);
+    posenet.on('pose', gotresult);
 }
-function draw(){
-image(video,0,0,750,400)
+
+function draw() {
+    image(video, 0, 0, 400, 400);
 }
-function piclol(){
- save('picwithmustachelol');
+
+function piclol() {
+    save('picwithmustachelol');
+}
+
+function modelloaded() {
+    console.log("model is loaded");
+}
+
+function gotresult(result) {
+    if (result.length > 0) {
+        console.log(result);
+        console.log("x="+result[0].pose.nose.x);
+        console.log("y="+result[0].pose.nose.y);
+    }
+
+
 }
