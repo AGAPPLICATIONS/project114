e8a5eeb217f968479348b7b4ad8da017a4fa0796
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-03-17 18:59:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+x=0;
+y=0;
 function preload() {
-
+ mustache=loadImage('https://i.postimg.cc/wjmYtCfv/mustache-removebg-preview.png');
 }
 
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.position(425,200);
     video = createCapture(VIDEO);
+    video.size(400,400)
     video.hide();
     posenet = ml5.poseNet(video, modelloaded);
     posenet.on('pose', gotresult);
@@ -13,6 +16,7 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 400, 400);
+    image(mustache,x,y,100,100);
 }
 
 function piclol() {
@@ -28,6 +32,8 @@ function gotresult(result) {
         console.log(result);
         console.log("x="+result[0].pose.nose.x);
         console.log("y="+result[0].pose.nose.y);
+        x=result[0].pose.nose.x - 50;
+        y=result[0].pose.nose.y - 35;
     }
 
 
